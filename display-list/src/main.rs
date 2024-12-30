bef09984162fcf8c6a6aa598acb33b9a74e748a9
Eduardo Oliveira <me@eduardooliveira.net>
2024-12-30 12:03:59 -0500

    minor changes to comments on display-list

--- display-list/src/main.rs
+++ display-list/src/main.rs
@@ -4,16 +4,14 @@ struct List(Vec<i32>);
 
 impl fmt::Display for List {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        // Extract the value using tuple indexing,
-        // and create a reference to `vec`.
+        // Extract the value using tuple indexing, and create a reference to `vec`.
         let vec: &Vec<i32> = &self.0;
 
         // The ? operator is used for error propagation
         // It will return the error if write! fails, otherwise continue execution
         write!(f, "[")?;
 
-        // Iterate over `v` in `vec` while enumerating the iteration
-        // count in `count`.
+        // Iterate over `v` in `vec` count in `count` while enumerating the iteration
         for (count, v) in vec.iter().enumerate() {
             // For every element except the first, add a comma.
             if count != 0 {
