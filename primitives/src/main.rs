89f96838b11e7e2de4862e18098cfe539034b0d5
Eduardo Oliveira <me@eduardooliveira.net>
2024-12-30 12:49:38 -0500

    literals and operators

--- primitives/src/main.rs
+++ primitives/src/main.rs
@@ -2,6 +2,7 @@ fn main() {
     primitives_scalar();
     primitives_compound();
     println!("---------");
+    literals_operators();
 }
 
 fn primitives_scalar() {
@@ -70,4 +71,42 @@ fn primitives_compound() {
     // and is constructed using parentheses ().
     let my_tuple: (u32, u8, bool, f32) = (5u32, 1u8, true, -5.04f32);
     println!("{:?}", my_tuple);
+}
+
+fn literals_operators() {
+    // Integers 1, floats 1.2, characters 'a', strings "abc", booleans true and the unit type () can be expressed using literals.
+
+    // Integers can, alternatively, be expressed using hexadecimal, octal or binary notation using these prefixes respectively: 0x, 0o or 0b.
+    // Underscores can be inserted in numeric literals to improve readability, e.g. 1_000 is the same as 1000, and 0.000_001 is the same as 0.000001.
+    // Rust also supports scientific E-notation, e.g. 1e6, 7.6e-4. The associated type is f64.
+
+    // We need to tell the compiler the type of the literals we use.
+    // For now, we'll use the u32 suffix to indicate that the literal is an unsigned 32-bit integer, and the i32 suffix to indicate that it's a signed 32-bit integer.
+
+    // Integer addition
+    println!("1 + 2 = {}", 1u32 + 2);
+
+    // Integer subtraction
+    // note that changes `1i32` to `1u32` (unsigned) would result in an error 'this arithmetic operation will overflow'
+    println!("1 - 2 = {}", 1i32 - 2);
+
+    // Scientific notation
+    println!("1e4 is {}, -2.5e-3 is {}", 1e4, -2.5e-3);
+
+    // Short-circuiting boolean logic
+    println!("Boolean ops:");
+    println!("\ttrue AND false is {}", true && false);
+    println!("\ttrue OR false is {}", true || false);
+    println!("\tNOT true is {}", !true);
+
+    // Bitwise operations
+    println!("Bitwise ops:");
+    println!("\t0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
+    println!("\t0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
+    println!("\t0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
+    println!("\t1 << 5 is {}", 1u32 << 5);
+    println!("\t0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+
+    // Use underscores to improve readability!
+    println!("One million is written as {}", 1_000_000u32);
 }
