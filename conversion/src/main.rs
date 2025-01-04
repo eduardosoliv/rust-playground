136f6a7fb90320c48f70a8534c14bf0e89fba45d
Eduardo Oliveira <me@eduardooliveira.net>
2025-01-04 16:40:39 -0500

    conversion: TryFrom and TryInto

--- conversion/src/main.rs
+++ conversion/src/main.rs
@@ -1,7 +1,13 @@
 mod frominto;
+mod tryfrominto;
+
 use frominto::{from, into};
+use tryfrominto::{try_from, try_into};
 
 fn main() {
     from();
     into();
+    println!("------");
+    try_from();
+    try_into();
 }
