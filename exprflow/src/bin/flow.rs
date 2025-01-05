b9c7964bb81f8400628755ed8a1238e0dc3dd0c3
Eduardo Oliveira <me@eduardooliveira.net>
2025-01-05 16:36:59 -0500

    match and destruct tuples, arrays, enums

--- exprflow/src/bin/flow.rs
+++ exprflow/src/bin/flow.rs
@@ -3,6 +3,7 @@ use exprflow::flowcontrol::ifelse;
 use exprflow::flowcontrol::loops;
 use exprflow::flowcontrol::whiles;
 use exprflow::patternmatching::basicmatch;
+use exprflow::patternmatching::destruct;
 
 fn main() {
     ifelse::simple_example(5);
@@ -12,6 +13,7 @@ fn main() {
     ifelse::assign_example(5);
     ifelse::assign_example(15);
     println!("------");
+
     loops::simple_loop();
     println!("---");
     loops::loop_nesting();
@@ -30,7 +32,15 @@ fn main() {
     println!("---");
     forloops::for_iter_mut();
     println!("------");
+
     basicmatch::match_number();
     println!("---");
     basicmatch::match_bool_assign();
+    println!("------");
+
+    destruct::destruct_tuples();
+    println!("---");
+    destruct::destruct_array();
+    println!("---");
+    destruct::destruct_enums();
 }
---
+++ exprflow/src/patternmatching/destruct.rs
@@ -0,0 +1,88 @@
+pub fn destruct_tuples() {
+    let triple = (0, -2, 3);
+
+    println!("Tell me about {:?}", triple);
+
+    match triple {
+        // Destructure the second and third elements
+        (0, y, z) => println!("First is `0`, `y` is {:?}, and `z` is {:?}", y, z),
+        (1, ..) => println!("First is `1` and the rest doesn't matter"),
+        (.., 2) => println!("last is `2` and the rest doesn't matter"),
+        (3, .., 4) => println!("First is `3`, last is `4`, and the rest doesn't matter"),
+        // `..` can be used to ignore the rest of the tuple
+        _ => println!("It doesn't matter what they are"),
+        // `_` means don't bind the value to a variable
+    }
+}
+
+pub fn destruct_array() {
+    let array = [3, -2, 6];
+
+    match array {
+        // Binds the second and the third elements to the respective variables
+        [0, second, third] => println!("array[0] = 0, array[1] = {}, array[2] = {}", second, third),
+
+        // Single values can be ignored with _
+        [1, _, third] => println!(
+            "array[0] = 1, array[2] = {} and array[1] was ignored",
+            third
+        ),
+
+        // You can also bind some and ignore the rest
+        [-1, second, ..] => println!(
+            "array[0] = -1, array[1] = {} and all the other ones were ignored",
+            second
+        ),
+        // The code below would not compile
+        // [-1, second] => ...
+
+        // Or store them in another array/slice (the type depends on
+        // that of the value that is being matched against)
+        [3, second, tail @ ..] => println!(
+            "array[0] = 3, array[1] = {} and the other elements were {:?}",
+            second, tail
+        ),
+
+        // Combining these patterns, we can, for example, bind the first and
+        // last values, and store the rest of them in a single array
+        [first, middle @ .., last] => println!(
+            "array[0] = {}, middle = {:?}, array[2] = {}",
+            first, middle, last
+        ),
+    }
+}
+
+#[allow(dead_code)]
+enum Color {
+    // These 3 are specified solely by their name.
+    Red,
+    Blue,
+    Green,
+    // These likewise tie `u32` tuples to different names: color models.
+    RGB(u32, u32, u32),
+    HSV(u32, u32, u32),
+    HSL(u32, u32, u32),
+    CMY(u32, u32, u32),
+    CMYK(u32, u32, u32, u32),
+}
+
+pub fn destruct_enums() {
+    let color = Color::RGB(122, 17, 40);
+
+    println!("What color is it?");
+    // An `enum` can be destructured using a `match`.
+    match color {
+        Color::Red => println!("The color is Red!"),
+        Color::Blue => println!("The color is Blue!"),
+        Color::Green => println!("The color is Green!"),
+        Color::RGB(r, g, b) => println!("Red: {}, green: {}, and blue: {}!", r, g, b),
+        Color::HSV(h, s, v) => println!("Hue: {}, saturation: {}, value: {}!", h, s, v),
+        Color::HSL(h, s, l) => println!("Hue: {}, saturation: {}, lightness: {}!", h, s, l),
+        Color::CMY(c, m, y) => println!("Cyan: {}, magenta: {}, yellow: {}!", c, m, y),
+        Color::CMYK(c, m, y, k) => println!(
+            "Cyan: {}, magenta: {}, yellow: {}, key (black): {}!",
+            c, m, y, k
+        ),
+        // Don't need another arm because all variants have been examined
+    }
+}
